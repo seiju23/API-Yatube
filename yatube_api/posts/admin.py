cebc1750936342ccaae66a497b3984590b644380
Игорь Равлис <ivravlis@mail.ru>
2023-04-30 17:12:33 +0300

    Second commit

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Group, Comment, Follow
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'description'
+    )
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'post', 'text', 'author', 'created'
+    )
+    search_fields = ('text', 'author', 'post')
+    list_filter = ('created', 'author')
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
